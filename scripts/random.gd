871c5aab7b9ea9ec8010067171828ff4d1af8382
Alain Hernandez <aehernandez@users.noreply.github.com>
2023-05-10 02:28:34 -0400

    Add choice/choices utility
    These functions mirror python's behavior.

--- scripts/random.gd
+++ scripts/random.gd
@@ -11,30 +11,48 @@ class_name Random
 #
 # Keys can be any value but values must be `int` types
 static func get_weighted_choice(prob_map: Dictionary):
-    var total: int = 0
-    for value in prob_map.values():
-        total += value
+	var total: int = 0
+	for value in prob_map.values():
+		total += value
 
-    var count: int = 0
-    var target = randi_range(0, total - 1)
+	var count: int = 0
+	var target = randi_range(0, total - 1)
 
-    for key in prob_map.keys():
-        var prob: int = prob_map[key]
+	for key in prob_map.keys():
+		var prob: int = prob_map[key]
 
-        var start = count
-        var end = prob + count
+		var start = count
+		var end = prob + count
 
-        if target >= start and target < end:
-            return key
+		if target >= start and target < end:
+			return key
 
-        count += prob
-        
-    # unreachable
-    assert(false)
+		count += prob
+		
+	# unreachable
+	assert(false)
 
 # Returns a random element in sequence
+# This method is deprecated, prefer using `Array.choice`
 static func get_random_choice(sequence):
-    assert(sequence.size() > 0)
-    var total = sequence.size()
-    var target = randi_range(0, total - 1)
-    return sequence[target]
+	assert(sequence.size() > 0)
+	var total = sequence.size()
+	var target = randi_range(0, total - 1)
+	return sequence[target]
+
+static func choice(sequence):
+	return sequence.pick_random()
+
+static func choices(sequence, n: int):
+	assert(n > 0)
+	assert(sequence.size() > 0)
+	assert(n <= sequence.size())
+	var samples_idx = range(sequence.size())
+	samples_idx.shuffle()
+	return samples_idx.slice(0, n).map(func(idx): return sequence[idx])
+
+static func vector2(from: float, to: float) -> Vector2:
+	return Vector2(
+		randf_range(from, to),
+		randf_range(from, to)
+	)
