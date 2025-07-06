065bbf124d75a5d3a8ba430aaa8c8b5eb6da78ce
Alain Hernandez <aehernandez@users.noreply.github.com>
2025-07-06 13:07:51 -0400

    Refactor + update outline shader

--- scripts/find.gd
+++ scripts/find.gd
@@ -12,12 +12,3 @@ static func nearest(from: Vector2, objects: Array) -> Node2D:
 			nearest_distance = distance
 
 	return nearest_node
-
-static func get_all_children(parent: Node) -> Array:
-	var children = []
-	var waiting := parent.get_children()
-	while not waiting.is_empty():
-		var node := waiting.pop_back() as Node
-		waiting.append_array(node.get_children())
-		children.push_back(node)
-	return children
--- scripts/node_util.gd
+++ scripts/node_util.gd
@@ -10,3 +10,21 @@ static func disconnect_all(s: Signal):
 	for connection in s.get_connections():
 		s.disconnect(connection.callable)
 	
+static func get_first_child_by_type(node: Node, type: Variant):
+	for child in node.get_children():
+		if is_instance_of(child, type):
+			return child
+	return null
+
+static func rotate_up_to_direction(node: Node2D, direction: Vector2):
+	node.look_at(node.global_position + direction)
+	node.rotate(deg_to_rad(90))
+
+static func get_all_children(parent: Node) -> Array:
+	var children = []
+	var waiting := parent.get_children()
+	while not waiting.is_empty():
+		var node := waiting.pop_back() as Node
+		waiting.append_array(node.get_children())
+		children.push_back(node)
+	return children
